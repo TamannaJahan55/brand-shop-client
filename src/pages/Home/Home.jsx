e4896734229d516b9486dfbdb4fcd784a43d05fe
TamannaJahan55 <tamannajahan.mis049@gmail.com>
2023-10-18 22:18:23 +0600

    navbar and banner page created and displayed in UI

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
+import Banner from "../Shared/Banner";
+import Navbar from "../Shared/Navbar";
 
 
 const Home = () => {
     return (
         <div>
+            <Navbar></Navbar>
+            <Banner></Banner>
             <h2>This is home</h2>
         </div>
     );
